bc07b06870eb02a8a70e5cf6737a0b5ce36ae3c0
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-11-11 09:02:58 -0500

    commit tree db03db6628042d5a75267eeb5381e46b46aa9ba2

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your code here!
+// Write your code here! 
+let element = document.createElement('div') 
+
+document.body.appendChild(element)  
+
+let ul = document.createElement('ul')
+ 
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li')
+  li.innerHTML = (i + 1).toString()
+  ul.appendChild(li)
+}
+ 
+element.appendChild(ul) 
+
+
+
+// Remove DOM node "main"
+document.getElementById("main").remove(); 
+
+// create new Element <h1>
+let newHeader = document.createElement("h1"); 
+
+// set <h1> with atrribute "id" and id#name to "victory" 
+newHeader.setAttribute("id", "victory"); 
+
+// set  
+newHeader.innerHTML = "Ian is the champion";
